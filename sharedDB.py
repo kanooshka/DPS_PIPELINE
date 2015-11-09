c287762be15f7f092d6698589349b761680c7652
kanooshka <kanooshka@gmail.com>
2015-11-09 09:32:48 -0500

    Added currentdate to sharedDB

--- sharedDB.py
+++ sharedDB.py
@@ -15,7 +15,7 @@ from DPSPipeline.database import connection
 from DPSPipeline import selection
 
 from PyQt4.QtCore   import QDate
-from datetime import datetime,timedelta
+from datetime import date,datetime,timedelta
 
 myVersion = version.Version("0.1.02")
 
@@ -31,6 +31,8 @@ autoCreateShotTasks = 0
 testPrivileges = 0
 testuser = 0
 #freezeDBUpdates = 1
+
+currentDate = date.today()
 
 earliestDate = QDate.currentDate().toPyDate()
 
