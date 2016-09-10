4bc21883d4e21f2c84e297b840813e11c0d430c6
kanooshka <kanooshka@gmail.com>
2016-09-10 14:33:12 -0400

    User Assignment Bug
    Fixed bug where user assignment would be set to assigned when setting
    hours on phase assignment

--- DPSPipeline/widgets/UserAssignmentSpinBox.py
+++ DPSPipeline/widgets/UserAssignmentSpinBox.py
@@ -12,8 +12,11 @@ class UserAssignmentSpinBox(QtGui.QSpinBox):
         self._parent = _parent
         
         self.setValue(0)
+	self.setMaximum(99999)
         self.getUserAssignment()
         
+	
+	
         #self._parent.totalHoursChanged
         self.valueChanged.connect(self.setHours)
         sharedDB.mySQLConnection.newUserAssignmentSignal.connect(self.CheckForUserAssignment)
@@ -56,14 +59,15 @@ class UserAssignmentSpinBox(QtGui.QSpinBox):
         elif self.value()>0:
             self._userAssignment =sharedDB.userassignments.UserAssignment(_idusers = self._user._idusers,_assignmentid = self._phaseAssignment._idphaseassignments,_assignmenttype = "phase_assignment",_idstatuses = 1, _hours = self.value(), _new = 1)
 	    
-	    self._phaseAssignment.setAssigned(1)
-            self._phaseAssignment.updateAssigned()
             self._userAssignment.userAssignmentAdded.connect(sharedDB.myTasksWidget.AddUserAssignment)
             #sharedDB.myUserAssignments.append(self._userAssignment)
             #connect to update
             self._userAssignment.userAssignmentChanged.connect(self.getHours)
 	    self._userAssignment.Save()
 	    self._userAssignment.connectToDBClasses()
+	    
+	    self._phaseAssignment.setAssigned(1)
+            self._phaseAssignment.updateAssigned()
 	    
     def wheelEvent(self, event):
         pass
